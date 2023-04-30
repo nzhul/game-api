a08275d306b1dc1e9576594c810ca7584119a7b7
nzhul <dobromirivanov1@gmail.com>
2023-04-30 16:09:30 +0300

    try to fix folder casing

---
+++ Server.Data/EntityConfigurations/MessageConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Server.Data.Users;
+
+namespace Server.Data.EntityConfigurations
+{
+    public class MessageConfig : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessagesRecieved)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
