9519781fb736439e506b30d886b6105fb9122a8e
nzhul <dobromirivanov1@gmail.com>
2023-05-10 22:09:27 +0300

    add root controller

---
+++ Server.Api/Controllers/RootController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Api.Controllers
+{
+    [AllowAnonymous]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Server is running.");
+        }
+    }
+}
